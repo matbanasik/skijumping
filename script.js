a94e6c3646dec6175f881284ff5855b1e1ebc894
matbanasik <mateuszbanasik22@gmail.com>
2017-10-18 17:43:34 +0200

    s

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ close.addEventListener('click', closeSlideMenu);
 
 
 function openSlideMenu(){
-	document.getElementById('side-menu').style.width = '250px';
-	document.querySelector('body').style.marginLeft = '250px';
+	document.getElementById('side-menu').style.display = 'block';
+	
 }
 
 function closeSlideMenu(){
-	document.getElementById('side-menu').style.width = '0';
-	document.querySelector('body').style.marginLeft = 'auto';
+	document.getElementById('side-menu').style.display = 'none';
+
 }
